d39b21110b06b439ee7dcd06a725eeec96d47145
Athenais83 <athenaisrudolf@gmail.com>
2024-02-22 19:38:43 +0100

    numération slide

--- my-app/src/components/SlideShow.jsx
+++ my-app/src/components/SlideShow.jsx
@@ -1,28 +1,37 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import '../sass/slide.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
+
 const SlideShow = ({ pictures }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    const CustomPrevArrow = (props) => (
-        <button {...props} className='custom-prev-arrow' onClick={() => setCurrentIndex(prevIndex => (prevIndex - 1 + pictures.length) % pictures.length)}>
-            <FontAwesomeIcon icon={faChevronLeft}/>
-        </button>
-    );
+    const goToPreviousSlide = () => {
+        setCurrentIndex(prevIndex => (prevIndex - 1 + pictures.length) % pictures.length);
+    };
 
-    const CustomNextArrow = (props) => (
-        <button {...props} className='custom-next-arrow' onClick={() => setCurrentIndex(prevIndex => (prevIndex + 1) % pictures.length)}>
-            <FontAwesomeIcon icon={faChevronRight}/>
-        </button>
-    );
+    const goToNextSlide = () => {
+        setCurrentIndex(prevIndex => (prevIndex + 1) % pictures.length);
+    };
+
+    useEffect(() => {
+        const updateSlideNumber = () => {
+            document.querySelector(".slide-number").innerText = `${currentIndex + 1}/${pictures.length}`;
+        };
+        updateSlideNumber();
+    }, [currentIndex, pictures.length]);
 
     return (
         <div className='carousel'>
-            <CustomPrevArrow />
+            <button className='custom-prev-arrow' onClick={goToPreviousSlide}>
+                <FontAwesomeIcon icon={faChevronLeft}/>
+            </button>
             <img src={pictures[currentIndex]} alt={""}/>
-            <CustomNextArrow />
+            <button className='custom-next-arrow' onClick={goToNextSlide}>
+                <FontAwesomeIcon icon={faChevronRight}/>
+            </button>
+            <div className="slide-number">{currentIndex + 1}/{pictures.length}</div>
         </div>
     );
 };
