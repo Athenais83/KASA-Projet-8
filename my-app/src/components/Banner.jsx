ea1e145fab1a3c9a06d5b6227be570540f60e91e
Athenais83 <athenaisrudolf@gmail.com>
2024-02-22 10:13:04 +0100

    css banner et card

--- my-app/src/components/Banner.jsx
+++ my-app/src/components/Banner.jsx
@@ -4,10 +4,12 @@ import "../App"
 function Banner ({imageUrl, text}) {
 
     return(
-        <div>
+        <div className="banner">
             <img src={imageUrl} alt={"Image" + imageUrl} className="image-banner"/>
+            <div className="overlay"></div>
             <p className="text-image">{text}</p>
         </div>
+        
     )
 }
 export default Banner;
