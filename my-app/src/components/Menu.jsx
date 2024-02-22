9e97c77ac4fa72a0b24653a4c25229306550948e
Athenais83 <athenaisrudolf@gmail.com>
2024-02-22 17:09:54 +0100

    css menu

--- my-app/src/components/Menu.jsx
+++ my-app/src/components/Menu.jsx
@@ -1,11 +1,12 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import '../sass/_menu.scss'
 
 const Menu = () => {
+    const location = useLocation();
     return(
         <nav className='menu'>
-            <Link to="/" className='link'>Accueil</Link>
-            <Link to="/About" className='link'>A propos</Link>
+            <Link to="/" className={location.pathname === '/' ? 'link active' : 'link'}>Accueil</Link>
+            <Link to="/About" className={location.pathname === '/About' ? 'link active' : 'link'}>A propos</Link>
         </nav>
     )
 }
